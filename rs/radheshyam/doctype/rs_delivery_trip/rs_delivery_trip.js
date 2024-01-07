5243beb40e5a2523bcdd977be87b5e61aeadf040
Pankaj <frappeuser@ip-172-31-22-128.ap-south-1.compute.internal>
2024-01-07 05:55:33 +0000

    7.1.24 from server

---
+++ rs/radheshyam/doctype/rs_delivery_trip/rs_delivery_trip.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Pankaj and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("RS Delivery Trip", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ rs/radheshyam/doctype/rs_delivery_trip/test_rs_delivery_trip.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Pankaj and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRSDeliveryTrip(FrappeTestCase):
+	pass
